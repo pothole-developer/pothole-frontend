2282c12e773499305240a0d1d2246586a4c4fea5
donghyuk <donghyuk454@gmail.com>
2024-04-30 18:37:19 +0900

    [Feat] baseURL 변경

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -9,8 +9,7 @@ export const authenticated = axios.create({
 });
 
 export const unauthenticated = axios.create({
-  //baseURL: process.env.REACT_APP_API_BASE_URL,
-  baseURL: 'http://localhost:3000',
+  baseURL: process.env.REACT_APP_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
