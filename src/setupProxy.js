3f87a5541657e626001befcd307c41def52775bb
donghyuk <donghyuk454@gmail.com>
2024-05-10 22:01:00 +0900

    [Feat] 프록시 재설정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,8 +2,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware('/potholes', {
-      target: process.env.REACT_APP_API_BASE_URL,
+    createProxyMiddleware('/pothole/v1/manager/potholes', {
+      target: process.env.REACT_APP_API_PROXY_BASE_URL,
       changeOrigin: true,
       secure: false
     }),
