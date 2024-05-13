9914520989fd1740848ebe926cae9975f44b84a4
donghyuk <donghyuk454@gmail.com>
2024-05-13 21:52:27 +0900

    [Feat] Map Style 적용 (+Container)

---
+++ src/components/map/style.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const MapContainer = styled.div`
+  height: 90vh;
+  width: 70vw;
+  border-right: groove;
+`;
+
+export const MapDiv = styled.div`
+  width: '100%';
+  height: 90vh;
+`;
