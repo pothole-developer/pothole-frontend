05381453d8e891dc0d103b35ff5a6a092913de81
donghyuk <donghyuk454@gmail.com>
2024-05-20 01:16:02 +0900

    [Feat] 필터용 style 개발 (공통)

---
+++ src/components/filter/style.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const FilterMainContainer = styled.div`
+  display:flex;
+  flex-direction: row;
+`;
+
+export const FilterMenuContainer = styled.div`
+  display:flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FilterResultContainer = styled.div`
+  display:flex;
+  flex-direction: column;
+`;
+
+export const FilterTitle = styled.text`
+  margin: 10px 10px 10px 10px;
+`;
+
+export const FilterComponent = styled.div`
+  margin: 5px 5px 5px 5px;
+`;
+
+export const FilterResult = styled.div`
+  background-color: lightgray;
+  padding: 5px 5px 5px 5px;
+  margin: 3px 3px 3px 3px;
+  border-radius: 10px;
+`;
