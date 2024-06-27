377010190987f7c468e8bcf24be2b081d28079fc
vinivin153 <vinivin153@naver.com>
2024-06-27 20:56:36 +0900

    [POT-59][Refactor] 오탈자 수정

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,12 @@
 import LoginForm from 'components/login/LoginForm.tsx';
-import { AppTtile } from 'components/title/AppTitle';
+import { AppTitle } from 'components/title/AppTitle';
 import { LoginPageContainer } from './style';
 
 export const LoginPage = () => {
   return (
     <LoginPageContainer>
-      <AppTtile />
+      <AppTitle />
       <LoginForm />
     </LoginPageContainer>
-  )
+  );
 };
