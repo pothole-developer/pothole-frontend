102eb3b1596ab65651bb08a35549e3d149c760dc
vinivin153 <vinivin153@naver.com>
2024-06-27 17:08:52 +0900

    [POT-58][Feat] 프록시 설정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function (app) {
+module.exports = (app) => {
   app.use(
-    createProxyMiddleware('/pothole/v1/manager/potholes', {
+    '/pothole',
+    createProxyMiddleware({
       target: process.env.REACT_APP_API_PROXY_BASE_URL,
       changeOrigin: true,
-      secure: false
     }),
   );
 };
