f658662509b982fa078d1a59990bf923f6b8a561
vinivin153 <vinivin153@naver.com>
2024-07-02 18:25:00 +0900

    [POT-59][Refactor] 네이버 API 최적화

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,90 +1,48 @@
-import { useState, useEffect } from 'react';
-import { setMarker, setPolygon } from '../../utils/naverMapUtils.tsx';
+import { useCallback, useEffect } from 'react';
+import { setMarker } from 'utils/naverMapUtils.tsx';
 import { MapContainer, MapDiv } from './style.tsx';
 
-interface MapProps {
-    position: Position;
-    markers: Position[];
-    polygon: PolygonProps;
-}
-
-interface PolygonProps {
-    paths: Position[];
-    fillColor: string;
-    fillOpacity: number;
-    strokeColor: string;
-    strokeOpacity: number;
-    strokeWeight: number;
-}
-
 interface Position {
-    latitude: number;
-    longitude: number;
+  latitude: number;
+  longitude: number;
 }
 
-let mapInstance: naver.maps.Map;
-
-const loadScript = (src: string, callback: () => void) => {
-  const script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = src;
-  script.onload = () => callback();
-  document.head.appendChild(script);
-};
-
-export const Map = ({position, markers, polygon} : MapProps) => {
-    // 지도 로딩 상태
-    const [isMapLoaded, setMapLoaded] = useState(false);
-
-    const initMap = () => {
-        // 추가 옵션 설정
-        const mapOptions = {
-            zoomControl: true,
-            zoomControlOptions: {
-                style: naver.maps.ZoomControlStyle.SMALL,
-                position: naver.maps.Position.TOP_LEFT,
-            },
-            center: new naver.maps.LatLng(position.latitude, position.longitude),
-            zoom: 16,
-        };
-        
-        // 지도 초기화 확인
-        if (document.getElementById('map')) {
-            mapInstance = new naver.maps.Map('map', mapOptions);
-        }
-
-        // Marker 생성
-        markers.forEach(m => setMarker(m, mapInstance));
-
-        // Polygon 세팅
-        setPolygon(polygon, mapInstance);
-
-        // 지도 로드 완료
-        setMapLoaded(true);
+export const Map = ({ markers }: { markers: Position[] }) => {
+  const initMap = useCallback(() => {
+    // 추가 옵션 설정
+    const mapOptions = {
+      zoomControl: true,
+      zoomControlOptions: {
+        style: naver.maps.ZoomControlStyle.SMALL,
+        position: naver.maps.Position.TOP_LEFT,
+      },
+      center: new naver.maps.LatLng(37.646339416503906, 127.02169036865234),
+      zoom: 16,
     };
 
-    useEffect(() => {
-        // 스크립트 로딩 확인
-        if (typeof naver === 'undefined' && typeof process.env.REACT_APP_NAVER_MAP_URL !== 'undefined') {
-            loadScript(
-                process.env.REACT_APP_NAVER_MAP_URL,
-                initMap,
-            );
-        } else {
-            initMap();
-        }
-    }, [position.latitude, position.longitude, isMapLoaded, markers]);
+    // 지도 초기화 확인
+    const mapInstance = new naver.maps.Map('map', mapOptions);
 
-    return (
-        <>
-        {/* 위치 정보(지도) */}
-        <div>
-            {isMapLoaded && (
-                <MapContainer>
-                    <MapDiv id="map" />
-                </MapContainer>
-            )}
-        </div>
-        </>
-    );
-}
+    // Marker 생성
+    markers.forEach((m) => setMarker(m, mapInstance));
+  }, [markers]);
+
+  useEffect(() => {
+    if (typeof process.env.REACT_APP_NAVER_MAP_URL !== 'undefined') {
+      const naverMapScript = document.createElement('script');
+      naverMapScript.onload = initMap;
+      naverMapScript.src = process.env.REACT_APP_NAVER_MAP_URL;
+      document.head.appendChild(naverMapScript);
+
+      return () => {
+        document.head.removeChild(naverMapScript);
+      };
+    }
+  }, [initMap]);
+
+  return (
+    <MapContainer>
+      <MapDiv id="map" />
+    </MapContainer>
+  );
+};
