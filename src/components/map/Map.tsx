63a245a83513794dabbd652a59bf873d69874ca0
vinivin153 <vinivin153@naver.com>
2024-07-02 21:45:27 +0900

    [POT-64][Feat] 보이는 지도 영역의 마커 표시 기능 구현

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,13 +1,15 @@
 import { useCallback, useEffect } from 'react';
-import { setMarker } from 'utils/naverMapUtils.tsx';
 import { MapContainer, MapDiv } from './style.tsx';
+import { getPotholeInfoContent } from 'utils/potholeInfo.ts';
 
 interface Position {
   latitude: number;
   longitude: number;
 }
 
-export const Map = ({ markers }: { markers: Position[] }) => {
+type IMarker = naver.maps.Marker;
+
+export const Map = ({ markersPos }: { markersPos: Position[] }) => {
   const initMap = useCallback(() => {
     // 추가 옵션 설정
     const mapOptions = {
@@ -23,9 +25,54 @@ export const Map = ({ markers }: { markers: Position[] }) => {
     // 지도 초기화 확인
     const mapInstance = new naver.maps.Map('map', mapOptions);
 
-    // Marker 생성
-    markers.forEach((m) => setMarker(m, mapInstance));
-  }, [markers]);
+    naver.maps.Event.addListener(mapInstance, 'zoom_changed', function () {
+      updateMarkers();
+    });
+
+    naver.maps.Event.addListener(mapInstance, 'dragend', function () {
+      updateMarkers();
+    });
+
+    const markers: IMarker[] = [];
+    const infoWindows = [];
+
+    for (const markerPos of markersPos) {
+      const { latitude, longitude } = markerPos;
+      const marker = new naver.maps.Marker({
+        map: mapInstance,
+        position: new naver.maps.LatLng(latitude, longitude),
+      });
+
+      const infoWindow = new naver.maps.InfoWindow({
+        content: getPotholeInfoContent({ latitude, longitude }),
+      });
+
+      markers.push(marker);
+      infoWindows.push(infoWindow);
+    }
+
+    const updateMarkers = () => {
+      const mapBounds = mapInstance.getBounds();
+
+      for (const marker of markers) {
+        const position = marker.getPosition();
+
+        if (mapBounds.hasPoint(position)) {
+          showMarker(marker);
+        } else {
+          hideMarker(marker);
+        }
+      }
+    };
+
+    const showMarker = (marker: IMarker) => {
+      marker.setMap(mapInstance);
+    };
+
+    const hideMarker = (marker: IMarker) => {
+      marker.setMap(null);
+    };
+  }, [markersPos]);
 
   useEffect(() => {
     if (typeof process.env.REACT_APP_NAVER_MAP_URL !== 'undefined') {
