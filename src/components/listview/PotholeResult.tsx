15c83422e98a8ba240acd9a3b99b6958bf64da9f
vinivin153 <vinivin153@naver.com>
2024-07-04 22:08:56 +0900

    [POT-67][Feat] 포트홀 갯수 전체에서 지도에서 보이는 포트홀 갯수로 변경

--- src/components/listview/PotholeResult.tsx
+++ src/components/listview/PotholeResult.tsx
@@ -1,9 +1,12 @@
+import { usePotholesStore } from 'hooks/usePotholesStore';
 import { ResultContainer, ResultTitle } from './style';
 
-export const PotholeResult = ({ numPotholes }: { numPotholes: number }) => {
+export const PotholeResult = () => {
+  const visiblePotholes = usePotholesStore((state) => state.visiblePotholes);
+
   return (
     <ResultContainer>
-      <ResultTitle>포트홀 갯수 : {numPotholes}개</ResultTitle>
+      <ResultTitle>포트홀 갯수 : {visiblePotholes.length}개</ResultTitle>
     </ResultContainer>
   );
 };
