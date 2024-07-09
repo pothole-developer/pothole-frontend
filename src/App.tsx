12a780d2cfac2c5a8cb4e6b74b4610cb9b360b15
vinivin153 <vinivin153@naver.com>
2024-07-09 22:00:09 +0900

    [POT-53][Feat] 글로벌 색상 설정

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,23 @@ import { HashRouter, Route, Routes } from 'react-router-dom';
 import { Main } from './pages/main/index.tsx';
 import { LoginPage } from 'pages/login/index.tsx';
 import ProtectedRoute from 'pages/routes/ProtectedRoute.tsx';
+import { ThemeProvider } from 'styled-components';
+import { colors } from 'styles/theme.ts';
 
 const App = () => {
   return (
-    <HashRouter>
-      <Routes>
-        <Route element={<ProtectedRoute />}>
-          <Route path="/main" element={<Main />}/>
-        </Route>
-        <Route path="/" element={<LoginPage />}/>
-        <Route path="/signup" element={<LoginPage />}/>
-      </Routes>
-    </HashRouter>
+    <ThemeProvider theme={colors}>
+      <HashRouter>
+        <Routes>
+          <Route element={<ProtectedRoute />}>
+            <Route path="/main" element={<Main />} />
+          </Route>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/signup" element={<LoginPage />} />
+        </Routes>
+      </HashRouter>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
