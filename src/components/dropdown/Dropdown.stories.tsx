b46c3da8957e57ece303037a909ab5422feb9292
vinivin153 <vinivin153@naver.com>
2024-07-10 19:31:25 +0900

    [POT-53][Test] dropdown 컴포넌트 테스트

---
+++ src/components/dropdown/Dropdown.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Dropdown } from './Dropdown';
+
+const meta: Meta<typeof Dropdown> = {
+  title: 'Dropdown',
+  component: Dropdown,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Dropdown>;
+
+export const DropdownTest: Story = {
+  render: () => <Dropdown></Dropdown>,
+};
