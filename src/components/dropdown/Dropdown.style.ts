d8895be5af3399369a9ea8553b6c4895ab572b6e
vinivin153 <vinivin153@naver.com>
2024-07-16 20:38:42 +0900

    [POT-53][Design] 애니메이션 자연스럽게 수정

--- src/components/dropdown/Dropdown.style.ts
+++ src/components/dropdown/Dropdown.style.ts
@@ -2,27 +2,27 @@ import styled, { keyframes } from 'styled-components';
 import { colors } from 'styles/theme';
 
 export const DropdownWrapper = styled.div`
+  position: relative;
   width: fit-content;
-  overflow: hidden;
 `;
 
 const slideDown = keyframes`
   from {
+    opacity:0;
     transform: translateY(-100%);
   }to{
+    opacity:1;
     transform: translateY(0%);
   }
 `;
 
-export const DropdownList = styled.div`
-  width: auto;
-  position: relative;
+export const DropdownList = styled.div<{ $width: string }>`
+  width: ${({ $width }) => $width};
   display: flex;
   flex-direction: column;
   border: 1.8px solid ${colors.mainGrey};
   border-radius: 2px;
   height: fit-content;
-  list-style: none;
   box-sizing: border-box;
   margin-top: 2px;
   animation: ${slideDown} 0.5s ease;
@@ -35,4 +35,12 @@ export const DropdownItem = styled.span`
   padding-left: 10px;
   background: ${colors.mainWhite};
   cursor: pointer;
+`;
+
+export const DropDownPosition = styled.div`
+  position: absolute;
+  top: 30px;
+  width: 100%;
+  overflow: hidden;
+  z-index: 1;
 `;
