2e6188955c0c4d5eaa43284ff3f5a8dab4806b4e
vinivin153 <vinivin153@naver.com>
2024-07-17 18:35:24 +0900

    [POT-53][Feat] listview에 filter 컴포넌트 배치

--- src/components/listview/PotholeResult.stlye.ts
+++ src/components/listview/PotholeResult.stlye.ts
@@ -1,4 +1,5 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+import { colors } from 'styles/theme';
 
 export const ResultContainer = styled.div`
   display: flex;
@@ -12,4 +13,34 @@ export const ResultTitle = styled.span`
   font-weight: bold;
   margin-top: 5px;
   margin-bottom: 5px;
+`;
+
+const slideIn = keyframes`
+  from {
+    opacity:0;
+    transform: translateX(100%);
+  }
+  to {
+    opacity:1;
+    transform: translateX(0%);
+  }
+`;
+
+export const SidebarWrapper = styled.div`
+  position: absolute;
+  overflow: hidden;
+  top: 0;
+  right: 100%;
+  width: 80%;
+  height: 100%;
+  margin-right: 1.5px;
+`;
+
+export const SidebarPosition = styled.div`
+  width: 100%;
+  position: absolute;
+  background: ${colors.mainWhite};
+  animation: ${slideIn} ease 0.3s;
+  padding: 10px 0px;
+  z-index: 1;
 `;
