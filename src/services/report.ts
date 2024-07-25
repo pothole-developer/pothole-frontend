87a5c0a286279e2492e5c407dbf93fb10aa22401
donghyuk <donghyuk454@gmail.com>
2024-07-25 23:10:43 +0900

    [POT-62][Feat] report api 호출 기능 개발

---
+++ src/services/report.ts
@@ -0,0 +1,29 @@
+import { AxiosResponse } from 'axios';
+import { unauthenticated } from './axiosInstance';
+
+export interface ReportRequest {
+  startDate: string;
+  endDate: string;
+  period: '' | '' | '' | '';
+}
+
+interface ReportResponse {
+  isSuccess: boolean;
+  code: number;
+  message: string;
+  data: any[];
+}
+
+export const fetchReport = async (request: ReportRequest) => {
+  if (isInvalidDateFormat(request.startDate) || isInvalidDateFormat(request.endDate)) {
+    console.log('Input 오류 - 형식 불일치 ');
+    return null;
+  }
+  const response: AxiosResponse<ReportResponse> = await unauthenticated.get('/manager/pothole-report');
+  return response.data;
+};
+
+const isInvalidDateFormat = (date: string) => {
+  const regex = /^\d{4}-\d{2}-\d{2}$/;
+  return !regex.test(date);
+} 
