4953aadac89f38adb626873e8d50bb5b383e8939
donghyuk <donghyuk454@gmail.com>
2024-07-25 23:08:34 +0900

    [POT-62][Feat] 기본 테이블 컴포넌트 개발

---
+++ src/components/table/DefaultTable.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {TableContainer, Table, Th, Td} from './style';
+
+interface DefaultTableProps {
+  data: Array<{ [key: string]: any }>;
+}
+
+export const DefaultTable: React.FC<DefaultTableProps> = ({ data }) => {
+  const keys = Object.keys(data[0]);
+  
+  return (
+    <TableContainer>
+      <Table>
+        <thead>
+          <tr>
+            {keys.map((key) => (
+              <Th key={key}>{key}</Th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              {keys.map((key) => (
+                <Td key={key}>{item[key]}</Td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </TableContainer>
+  );
+};
